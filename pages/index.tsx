af5624734b512496ca60c27e30b03b54fe72f3ac
geritol <gergo.tolnai@gmail.com>
2021-04-17 11:12:15 +0200

    feat(landing): add minimal welcome text

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,20 @@ export default function Home() {
 
       <main className="container mx-auto text-center">
         <h1 className="text-4xl mt-24">Welcome to Közért!</h1>
+        <p className="mt-8 text-lg">
+          Közért is an open source online platform that helps NGOs <br /> to
+          recruit volunteers for their projects.
+        </p>
+        <p className="mt-6">
+          The project is under development, help us on{" "}
+          <a
+            className="text-blue-500 hover:underline"
+            href="https://github.com/geritol/kozert.org"
+            target="__blank"
+          >
+            Github
+          </a>
+        </p>
       </main>
     </div>
   );
