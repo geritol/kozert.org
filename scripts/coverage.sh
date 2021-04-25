62531e1c9f27bfbfe78acc96e4b1617658fe5562
geritol <gergo.tolnai@gmail.com>
2021-04-25 16:17:24 +0200

    ci: add coverage reporting to coveralls

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,3 +5,4 @@ npm run test -- --collect-coverage
 npm run test:integration -- --collect-coverage
 npx istanbul-merge --out ./.jest/coverage.json ./.jest/unit/coverage-final.json ./.jest/int/coverage-final.json
 npx istanbul report --reporter text --include ./.jest/coverage.json
+npx istanbul report --reporter lcov --include ./.jest/coverage.json --dir .jest
