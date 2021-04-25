182d0135ceba6d39e036ec7eb1c168d85cf6857a
geritol <gergo.tolnai@gmail.com>
2021-04-25 17:38:58 +0200

    feat(project): create basic project page

---
+++ backend/data/project.ts
@@ -0,0 +1,16 @@
+import { Project } from "@prisma/client";
+import { prisma } from "backend/db";
+
+export const getAllProjectIds = async (): Promise<number[]> => {
+  const projects = await prisma.project.findMany({ select: { id: true } });
+  return projects.map((p) => p.id);
+};
+
+export const getProject = async (
+  id: number | string
+): Promise<Project | undefined> => {
+  if (typeof id === "string") {
+    id = parseInt(id, 10);
+  }
+  return prisma.project.findUnique({ where: { id } });
+};
