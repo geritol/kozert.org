af467279484d68f2986bf1581d4b062be91fab18
geritol <gergo.tolnai@gmail.com>
2021-04-25 21:39:19 +0200

    fix(project-page): add h4 to markdown components

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -33,6 +33,7 @@ export default function ProjectView(props: { project: Project }) {
               h1: "h2",
               h2: "h3",
               h3: "h4",
+              h4: "h5",
               h5: "h6",
             }}
           >
